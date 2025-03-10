0a9f3895a5f2a82cf1ebd6ccfa23ddfa66de342b
caffine_Overflow <tnzajk@gmail.com>
2025-03-10 16:35:27 +0530

    parent app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import AdminLogin from "./components/Auth/AdminLogin";
+import Dashboard from "./components/pages/Dashboard";
+import MenuManagement from "./components/pages/MenuManagement";
+import ViewOrders from "./components/pages/ViewOrders";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={AdminLogin} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/menu" component={MenuManagement} />
+        <Route path="/viewOrders" component={ViewOrders} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
